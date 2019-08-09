674302f4df7189c5b488dbe14c5b57dee5c79d16
keenanSchramm <keenan.schramm@gmail.com>
2019-08-09 12:55:13 -0600

    first setup

---
+++ pageObjects/automationBasicsPageObjects.js
@@ -0,0 +1,12 @@
+module.exports = {
+    url: 'https://devmountain-qa.github.io/Automation-Basics/build/',
+    elements: {
+
+        //element: 'element selector',
+        //if using xpath exampleVV
+        // rVSeeAll: {
+        //     selector: '(//div[@class="sc-jqCOkK djSgtl sc-gqjmRU fmVgeN"])[1]',
+        //     locateStrategy: 'xpath'
+        // },
+    }
+}
