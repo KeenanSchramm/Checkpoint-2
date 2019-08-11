2e201addf017de60b617a268b7d1e0cb13a0ef45
keenanSchramm <keenan.schramm@gmail.com>
2019-08-11 03:28:37 -0600

    almost done

--- pageObjects/automationBasicsPageObjects.js
+++ pageObjects/automationBasicsPageObjects.js
@@ -1,5 +1,19 @@
 module.exports = {
     url: 'https://devmountain-qa.github.io/Automation-Basics/build/',
+    var: person1 = {
+        name: "Jimmy Joe",
+        title: "Hack0r",
+        age: "12"
+    },
+    var: person2 = {
+        name: "Jeremy Schrader",
+        hairColor: "brown",
+        age: "24"
+    },
+    var: person3 = {
+        name: "Carly Armstrong",
+        title: "CEO"
+    },
     elements: {
         evenOddInput: {
             selector: '//input[@name="evenOddInput"]',
@@ -16,7 +30,61 @@ module.exports = {
         oddResults: {
             selector: '//span[@name="oddResults"]',
             locateStrategy: 'xpath'
-        }
+        },
+        filterObjInput: {
+            selector: '//input[@name="objectFilterInput"]',
+            locateStrategy: 'xpath'
+        },
+        filterObjConfBtn: {
+            selector: '//button[@name="objectFilterButton"]',
+            locateStrategy: 'xpath'
+        },
+        filterObjResults: {
+            selector: '//span[@name="objectFilterResults"]',
+            locateStrategy: 'xpath'
+        },
+        filterNamInput: {
+            selector: '//input[@id="nameFilterInput"]',
+            locateStrategy: 'xpath'
+        },
+        filterNamConfBtn: {
+            selector: '//button[@id="nameFilterButton"]',
+            locateStrategy: 'xpath'
+        },
+        filterNamResults: {
+            selector:'//span[@name="nameFilterResults"]',
+            locateStrategy: 'xpath'
+        },
+        palInput: {
+            selector: '//input[@name="palindromeInput"]',
+            locateStrategy: 'xpath'
+        },
+        palConfBtn: {
+            selector: '//button[@name="palindromeButton"]',
+            locateStrategy: 'xpath'
+        },
+        palResults: {
+            selector:'//span[@name="palindromeResults"]',
+            locateStrategy: 'xpath'
+        },
+        sumInput1: {
+            selector: '//input[@name="sumInput1"]',
+            locateStrategy: 'xpath'
+        },
+        sumInput2: {
+            selector: '//input[@name="sumInput2"]',
+            locateStrategy: 'xpath'
+        },
+        sumConfBtn: {
+            selector: '//button[@name="sumButton"]',
+            locateStrategy: 'xpath'
+        },
+        sumResults: {
+            selector:'//span[@name="sumResults"]',
+            locateStrategy: 'xpath'
+        },
+        
+
         //element: 'element selector',
         //if using xpath exampleVV
         // rVSeeAll: {
--- tests/automationBasics.js
+++ tests/automationBasics.js
@@ -4,84 +4,190 @@ module.exports = {
         automationBasics = browser.page.automationBasicsPageObjects()
         automationBasics.navigate()
     },
+    //automationbasics reqs link https://devmountain-qa.github.io/Automation-Basics/
+    //automationbasics app https://devmountain-qa.github.io/Automation-Basics/build/
     'Odds and Evens correct input': browser => {
         automationBasics
-            .setValue('@evenOddInput','1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,100,101,102,103,104,105')
+            .setValue('@evenOddInput', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,100,101,102,103,104,105')
             .click('@evenOddConfBtn')
             .verify.containsText('@evenResults', "2,4,6,8,10,12,14,100,102,104")
-            .verify.containsText('@oddResults',"1,3,5,7,9,11,13,15,101,103,105")
+            .verify.containsText('@oddResults', "1,3,5,7,9,11,13,15,101,103,105")
             .expect.element('@oddResults').text.not.contain("2,4,6,8,10,12,14,100,102,104")
         automationBasics
             .expect.element('@evenResults').text.not.contain("1,3,5,7,9,11,13,15,101,103,105")
     },
 
-    // //'Odds and Evens incorrect input': browser => {
+    'Odds and Evens incorrect input': browser => {
+        automationBasics
+            .setValue('@evenOddInput', 'ABCD')
+            .click('@evenOddConfBtn')
+            .expect.element('@evenResults').text.to.equal("Evens: []")
+        automationBasics
+            .expect.element('@oddResults').text.to.equal("Odds: [null]")
+        automationBasics
+            .clearValue('@evenOddInput')
+            .pause(500)
+            .setValue('@evenOddInput', '!@#$%^&*')
+            .click('@evenOddConfBtn')
+            .expect.element('@evenResults').text.to.equal("Evens: []")
+        automationBasics
+            .expect.element('@oddResults').text.to.equal("Odds: [null]")
+    },
+    'Filter object acceptable name input': browser => {
+        automationBasics
+            .setValue('@filterObjInput', 'name')
+            .click('@filterObjConfBtn')
+            .verify.containsText('@filterObjResults', person1.name, person1.title)
+            .verify.containsText('@filterObjResults', person2.name, person2.age)
+            .verify.containsText('@filterObjResults', person3.name, person3.title)
+    },
+    'Filter object acceptable title input': browser => {
+        automationBasics
+            .setValue('@filterObjInput', 'title')
+            .click('@filterObjConfBtn')
+            .verify.containsText('@filterObjResults', person1.title, person1.name)
+            .verify.containsText('@filterObjResults', person3.title, person3.name)
+    },
+    'Filter object acceptable age input': browser => {
+        automationBasics
+            .setValue('@filterObjInput', 'age')
+            .click('@filterObjConfBtn')
+            .verify.containsText('@filterObjResults', person1.age)
+            .verify.containsText('@filterObjResults', person2.age)
+    },
+    'Filter Object acceptable hair color input': browser => {
+        automationBasics
+            .setValue('@filterObjInput', 'hairColor')
+            .click('@filterObjConfBtn')
+            .verify.containsText('@filterObjResults',
+                person2.hairColor,
+                person2.age,
+                person2.name)
+    },
+    'Filter Object unacceptable input': browser => {
+        automationBasics
+            .setValue('@filterObjInput', 'Object')
+            .click('@filterObjConfBtn')
+            .expect.element('@filterObjResults').text.to.not.contain(person1.name,
+                person1.title,
+                person1.age)
+        automationBasics
+            .expect.element('@filterObjResults').text.to.not.contain(person2.name,
+                person2.hairColor,
+                person2.age)
+        automationBasics
+            .expect.element('@filterObjResults').text.to.not.contain(person3.name,
+                person3.title)
+    },
+    'Filter String input': browser => {
+        automationBasics
+            .setValue('@filterNamInput', 'James')
+            .click('@filterNamConfBtn')
+            .verify.containsText('@filterNamResults','James')
+            .expect.element('@filterNamResults').text.to.not.contain("Jessica", "Melody", "Tyler", "Blake", "Jennifer", "Mark", "Maddy")
+        automationBasics
+            .clearValue('@filterNamInput')
+            .setValue('@filterNamInput', 'Jessica')
+            .click('@filterNamConfBtn')
+            .verify.containsText('@filterNamResults','Jessica')
+            .expect.element('@filterNamResults').text.to.not.contain("James", "Melody", "Tyler", "Blake", "Jennifer", "Mark", "Maddy")
+        automationBasics
+            .clearValue('@filterNamInput')
+            .setValue('@filterNamInput', 'Melody')
+            .click('@filterNamConfBtn')
+            .verify.containsText('@filterNamResults','Melody')
+            .expect.element('@filterNamResults').text.to.not.contain("James", "Jessica", "Tyler", "Blake", "Jennifer", "Mark", "Maddy")
+        automationBasics
+            .clearValue('@filterNamInput')
+            .setValue('@filterNamInput', 'Tyler')
+            .click('@filterNamConfBtn')
+            .verify.containsText('@filterNamResults','Tyler')
+            .expect.element('@filterNamResults').text.to.not.contain("James", "Jessica", "Melody", "Blake", "Jennifer", "Mark", "Maddy")
+        automationBasics
+            .clearValue('@filterNamInput')
+            .setValue('@filterNamInput', 'Blake')
+            .click('@filterNamConfBtn')
+            .verify.containsText('@filterNamResults','Blake')
+            .expect.element('@filterNamResults').text.to.not.contain("James", "Jessica", "Melody", "Tyler", "Mark", "Maddy")
+        automationBasics
+            .clearValue('@filterNamInput')
+            .setValue('@filterNamInput', 'Jennifer')
+            .click('@filterNamConfBtn')
+            .verify.containsText('@filterNamResults','Jennifer')
+            .expect.element('@filterNamResults').text.to.not.contain("James", "Jessica", "Melody", "Tyler", "Blake", "Mark", "Maddy")
+        automationBasics
+            .clearValue('@filterNamInput')
+            .setValue('@filterNamInput', 'Mark')
+            .click('@filterNamConfBtn')
+            .verify.containsText('@filterNamResults','Mark')
+            .expect.element('@filterNamResults').text.to.not.contain("James", "Jessica", "Melody", "Tyler", "Blake", "Jennifer", "Maddy")
+        automationBasics
+            .clearValue('@filterNamInput')
+            .setValue('@filterNamInput', 'Maddy')
+            .click('@filterNamConfBtn')
+            .verify.containsText('@filterNamResults','Maddy')
+            .expect.element('@filterNamResults').text.to.not.contain("James", "Jessica", "Melody", "Tyler", "Blake", "Jennifer", "Mark")
+    },
+    'Palindrome acceptable input': browser => {
+        automationBasics
+            .setValue('@palInput', "racecar")
+            .click('@palConfBtn')
+            .verify.containsText('@palResults', "true")
+            .clearValue('@palInput')
+            .setValue('@palInput', "madam")
+            .click('@palConfBtn')
+            .verify.containsText('@palResults', "true")
+            .clearValue('@palInput')
+            .setValue('@palInput', "kayak")
+            .click('@palConfBtn')
+            .verify.containsText('@palResults', "true")
+            .clearValue('@palInput')
+            .setValue('@palInput', "civic")
+            .click('@palConfBtn')
+            .verify.containsText('@palResults', "true")
+            .clearValue('@palInput')
+            .setValue('@palInput', "solos")
+            .click('@palConfBtn')
+            .verify.containsText('@palResults', "true")
+            .clearValue('@palInput')
+            .setValue('@palInput', "rotator")
+            .click('@palConfBtn')
+            .verify.containsText('@palResults', "true")
+            .clearValue('@palInput')
+            
+    },
+    'Palindrome unacceptable input': browser => {
+        automationBasics
+            .setValue('@palInput', "notapalindrome")
+            .click('@palConfBtn')
+            .verify.containsText('@palResults', "false")
+            .clearValue('@palInput')
+            .setValue('@palInput', "another")
+            .click('@palConfBtn')
+            .verify.containsText('@palResults', "false")
+            .clearValue('@palInput')
+            .setValue('@palInput', "phrase")
+            .click('@palConfBtn')
+            .verify.containsText('@palResults', "false")
+            .clearValue('@palInput')
+            .setValue('@palInput', "sword")
+            .click('@palConfBtn')
+            .verify.containsText('@palResults', "false")
+            .clearValue('@palInput')
+            .setValue('@palInput', "Saying All Sorts Of Random Things")
+            .click('@palConfBtn')
+            .verify.containsText('@palResults', "false")
+            .clearValue('@palInput')
+            .setValue('@palInput', "watermelon")
+            .click('@palConfBtn')
+            .verify.containsText('@palResults', "false")
+            .clearValue('@palInput')
+    },
+    // 'Sum': browser => {
     //     automationBasics
-                //input letters
-                //click sort
-                //check even output
-                //check odd output
     //         .click('selector')
     //         .pause(500)
     //         .waitForElementVisible('selector')
     //         .verify.containsText('selector', "text")
     // },
-
-    // //'test1': browser => {
-    //     automationBasics
-    //         .click('selector')
-    //         .pause(500)
-    //         .waitForElementVisible('selector')
-    //         .verify.containsText('selector', "text")
-    // },
-
-    // //'Filter Object': browser => {
-    //     automationBasics
-    //         .click('selector')
-    //         .pause(500)
-    //         .waitForElementVisible('selector')
-    //         .verify.containsText('selector', "text")
-    // },
-
-    // //'Filter String': browser => {
-    //     automationBasics
-    //         .click('selector')
-    //         .pause(500)
-    //         .waitForElementVisible('selector')
-    //         .verify.containsText('selector', "text")
-    // },
-
-    // //'Palindrome': browser => {
-    //     automationBasics
-    //         .click('selector')
-    //         .pause(500)
-    //         .waitForElementVisible('selector')
-    //         .verify.containsText('selector', "text")
-    // },
-
-    // //'Sum': browser => {
-    //     automationBasics
-    //         .click('selector')
-    //         .pause(500)
-    //         .waitForElementVisible('selector')
-    //         .verify.containsText('selector', "text")
-    // },
-
-
-
-
-
-
-
-
-
-    //test layout example
-    // //'test1': browser => {
-    //     automationBasics
-    //         .click('selector')
-    //         .pause(500)
-    //         .waitForElementVisible('selector')
-    //         .verify.containsText('selector', "text")
-    // },
-
 }
